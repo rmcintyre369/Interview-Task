bb2fbf9fecd81d3c958b98b1a10b71029aa0ac4a
rmcintyre369 <r.mcintyre369@gmail.com>
2025-02-02 20:59:28 +0000

    Refactored the code for Task 3 and added error handling. Wrote readme file for the repository.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Interview-Task
+
+This repository contains the solution for three interview tasks.
+
+## Task 1
+This task is about loading a JSON file, filtering the data based on book category and publish data, increasing the prices and finally writing the result to a new CSV file.
+After completing the initial sak, I have added functionality to allow the user to input the varaibles that will be used for the program.
+
+## Task 2
+This task had the same ask as in Task 1, but was to be completed by creating a scenario in MAKE. The MAKE scenario loads the JSON data using a HTTP request, filters the data based on category and publish data through a filter then uses the Math module to increase the prices then converts the data to CSV file and emails the result.
+
+## Task 3
+The final task was to create a python script that will read an excel file, convert it to JSON, sort the data based on a specific category, add a new field to the data and finally write the result to a new JSON file.
--- Task3/task3Solution.py
+++ Task3/task3Solution.py
@@ -1,40 +1,48 @@
-# ToDo refactoring, add aditional features and tests
-
 import pandas as pd
 import json
 
-url = 'https://d2hmvvndovjpc2.cloudfront.net/efe'
+URL = 'https://d2hmvvndovjpc2.cloudfront.net/efe'
+SORT_CATEGORY = 'levelColor'
+DATA_FILE = 'data.xlsx'
+OUTPUT_FILE = 'final_result.json'
 
 def ExcelToJSON(file_path):
+    try:
+        data = pd.read_excel(file_path)
+        json_data = data.where(pd.notnull(data), "").to_dict(orient='records')
+        return json_data
+    except Exception as e:
+        print(f"Error: An unexpected error occurred: {str(e)}")
+        return []
 
-    data = pd.read_excel(file_path)
+def json_writer(json_data, file_path):
+    try:
+        with open(file_path, 'w') as json_file:
+            json.dump(json_data, json_file, indent=4, default=str)
+    except Exception as e:
+        print(f"Error: An unexpected error occurred: {str(e)}")
 
-    json_data = data.where(pd.notnull(data), "").to_dict(orient='records')
-
-    with open('original_data.json', 'w') as json_file:
-        json.dump(json_data, json_file, indent=4, default=str)
-
-    return json_data
-
-def SortJSON(json_data):
-    sorted_json = sorted(json_data, key=lambda x: x['levelColor'], reverse=False)
-
-    with open('sorted_data.json', 'w') as json_file:
-        json.dump(sorted_json, json_file, indent=4, default=str)
-
-    return sorted_json
+def SortJSON(json_data, sort_category):
+    try:
+        sorted_json = sorted(json_data, key=lambda x: x[sort_category], reverse=False)
+        return sorted_json
+    except Exception as e:
+        print(f"Error: An unexpected error occurred: {str(e)}")
+        return []
 
 def addField(sorted_json, url):
-    for book in sorted_json:
-        full_url = f"{url.rstrip('/')}/{book.get('filepath').lstrip('/')}/{book.get('filename')}"
-        book.update({'url': full_url})
-
-    
-    with open('sorted_data_with_URL.json', 'w') as json_file:
-        json.dump(sorted_json, json_file, indent=4, default=str)
-
-    return sorted_json
+    try:
+        for book in sorted_json:
+            full_url = f"{url.rstrip('/')}/{book.get('filepath').lstrip('/')}/{book.get('filename')}"
+            book.update({'url': full_url})
+        return sorted_json
+    except Exception as e:
+        print(f"Error: An unexpected error occurred: {str(e)}")
+        return []
 
 if __name__ == "__main__":
-    addField(SortJSON(ExcelToJSON('data.xlsx')),url)
+    json_data = ExcelToJSON(DATA_FILE)
+    sorted_json = SortJSON(json_data, SORT_CATEGORY)
+    sorted_json_with_url = addField(sorted_json, URL)
+    json_writer(sorted_json_with_url, OUTPUT_FILE)
 
